ff190a607de0f2be218a4cbc5b606b2f538e3347
Patrick Bello <mayorbello@yahoo.com>
2021-03-12 20:59:53 -0700

    added contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ContactForm from './Form/ContactForm';
+import gsap from 'gsap';
+
+const animation = gsap.timeline();
 
 const Contact = () => {
 
+  useEffect(() => {
+    animation
+      .fromTo('.contact .svg', {bottom:'3%'}, {delay: 1, repeat: -1, ease: 'back', bottom:'10%', yoyo:true, duration: 2})
+}, [])
+
   return (
     <div className="contact">
-      Contact
+      <div className="top">
+        <div className="text">
+          <h1>Here's how you can reach me.</h1>
+        </div>
+      </div>
+      <div className="middle">
+        <a href="https://www.linkedin.com/in/patrick-bello-6796521b3/" target="_blank" rel="noreferrer">
+          <p className="text">LinkedIn</p>
+          <img style={{transitionDelay: '0.6s'}} src="https://img.icons8.com/color/64/000000/linkedin.png" alt="linkedin logo"/>
+        </a>
+        <a href="https://github.com/mayorbcode" target="_blank" rel="noreferrer">
+          <p className="text">GitHub</p>
+          <img style={{transitionDelay: '0.65s'}} src="https://img.icons8.com/fluent/64/000000/github.png" alt="github logo"/>
+        </a>
+        <a href="https://codepen.io/mayorbcode" target="_blank" rel="noreferrer">
+          <p className="text">Codepen</p>
+          <img style={{transitionDelay: '0.7s'}} src="https://img.icons8.com/material/64/ffffff/codepen.png" alt="codepen logo"/>
+        </a>
+      </div>
+      <div className="bottom">
+        <ContactForm />
+        <div className="svg">
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Form/ContactForm.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      <form
+        onSubmit={this.submitForm}
+        action="https://formspree.io/f/mbjpnadn"
+        method="POST"
+      >
+        <legend>Contact me!</legend>
+        <p>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="email"/>
+        </p>
+        <p>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" id="message"/>
+        </p>
+        {status === "SUCCESS" ? <p style={{color: 'white', textAlign: 'center', fontSize: '1.2rem'}}>Sent!</p> : <button>Send</button>}
+        {status === "ERROR" && <p style={{color: 'white', textAlign: 'center'}}>Ooops! There was an error.</p>}
+      </form>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
